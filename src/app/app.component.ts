7bd967972c8a2742384eb4e48501c6e35a723702
meetmahajan <monoarchos@gmail.com>
2019-09-11 13:27:01 +0530

    Notify Me--added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: ['./app.component.css']
 })
-export class AppComponent  {}
+export class AppComponent implements OnInit {
+  ngOnInit() {
+  }
+  // getData() {
+  //   window.alert("123");
+  //   console.log(3454534534)
+  // }
+}
 
 
 /*
